d3c38e85daa34f15c812db80759c00a2440ac375
Mehdi Poussier <mpoussie@c2r12p1.42lehavre.fr>
2023-10-17 08:53:56 +0200

    add

--- ft_str/ft_strchr.c
+++ ft_str/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mpoussie <mpoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 22:05:19 by mpoussie          #+#    #+#             */
-/*   Updated: 2023/10/17 08:51:59 by mpoussie         ###   ########.fr       */
+/*   Updated: 2023/10/17 08:53:45 by mpoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	int	i;
+	int		i;
+	char	cc;
 
+	cc = (char)c;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == (char)c)
+		if (str[i] == cc)
 			return ((char *)(str + i));
 		i++;
 	}
-	if (str[i] == (char)c)
-		return ((char *)(str + i));
 	return (NULL);
 }
